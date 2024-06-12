f1fba4d085d8c16080253bdc7060593f3eb53553
salvadoorssalinas <u20221b127@upc.edu.pe>
2024-06-11 19:52:52 -0500

    feat: cache data

--- pc202302/pc202302/obj/Debug/net8.0/pc202302.AssemblyInfo.cs
+++ pc202302/pc202302/obj/Debug/net8.0/pc202302.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pc202302")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ad3e1ad64c606935d293a4a033591b5f9dabf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e315c58034a54096f814b275e07878088b3c8039")]
 [assembly: System.Reflection.AssemblyProductAttribute("pc202302")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pc202302")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
